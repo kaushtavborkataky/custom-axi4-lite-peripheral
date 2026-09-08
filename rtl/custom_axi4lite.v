33c251c5367e19fc307847b3f6317e996c51f4fa
Kaushtav Borkataky <kaushtavborkataky@gmail.com>
2026-09-08 20:57:06 +0530

    Add files via upload

---
+++ rtl/custom_axi4lite.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.08.2026 17:00:02
+// Design Name: 
+// Module Name: custom_axi4lite
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module custom_axi4lite(
+
+    );
+endmodule
